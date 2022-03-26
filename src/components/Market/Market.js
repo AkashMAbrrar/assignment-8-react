e8329949b10173eabf923f9cf99bd58dad8211b6
AkashAbrrar <mabrrar9@gmail.com>
2022-03-26 13:57:49 +0600

    product information and icon added

--- src/components/Market/Market.js
+++ src/components/Market/Market.js
@@ -5,17 +5,27 @@ import './Market.css';
 const Market = () => {
     const [products, setProducts] = useState([]);
 
+    const [cart, setCart] = useState([]);
+
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
-    }, [])
+    }, []);
+
+    const addHandleClick = (product) => {
+        console.log(product)
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
     return (
         <div className='market-container'>
             <div className="products-container">
                 {
                     products.map(product => <Product
                         product={product}
+                        addHandleClick={addHandleClick}
                         key={product.id}
 
                     ></Product>)
@@ -23,6 +33,7 @@ const Market = () => {
             </div>
             <div className="cart-container">
                 <h3>Product Summary</h3>
+                <p>Product count: {cart.length}</p>
             </div>
         </div>
     );
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import './Product.css'
 
-const Product = (props) => {
-    const { name, brand, price, img } = props.product;
+const Product = ({ product, addHandleClick }) => {
+    const { name, brand, price, img } = product;
+
     return (
         <div className='product'>
             <img src={img} alt=''></img>
@@ -11,8 +14,10 @@ const Product = (props) => {
                 <h3>Brand: {brand}</h3>
                 <p>Price: $ {price}</p>
             </div>
-            <button className='btn-cart'>
-                <p>Add To Cart</p>
+
+            <button onClick={() => addHandleClick(product)} className='btn-cart'>
+                <p className='btn-text'>Add To Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
         </div>
     );
