a780835dfd79ad6e97a26292aa1ce822fa3bd3b7
AkashAbrrar <mabrrar9@gmail.com>
2022-03-26 10:40:41 +0600

    second commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Take Your Time For The Safe Journey</h1>
+            <span><h3>And Go Forward Easily</h3></span>
+        </div>
+    );
+};
+
+export default Header;
